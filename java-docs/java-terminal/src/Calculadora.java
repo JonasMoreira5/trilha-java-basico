b2d4a8ccb64987317beb4356f9ee1362b34772c4
Jonas Moreira <jonasmoreira076@gmail.com>
2023-07-05 12:15:24 -0300

    Seção de documentação de um calculadora em Java

---
+++ java-docs/java-terminal/src/Calculadora.java
@@ -0,0 +1,23 @@
+/**
+ * <h1>Calculadora</h1>
+ * A Calculadora realiza operações matemáticas entre números
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação desta classes
+ * para desfrutar dos recursos oferecidos pelo autor
+ * 
+ * @author Jonas Moreira
+ * @version 1.0
+ * @since 05/07/2023
+ */
+
+public class Calculadora {
+    /**
+    * Este método é utilizadfo para somar dois números inteiros 
+    * @param numeroUm este é o primeiro parâmetro do método
+    * @param numeroDois este é o segundo parâmetro do método
+    * @return int o resultado deste método é a soma dois números.
+    */
+    public int somar(int numeroUm, int numeroDois){
+        return numeroUm+numeroDois;
+    }    
+} 

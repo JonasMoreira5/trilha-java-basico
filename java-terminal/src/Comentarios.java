9d76bc23364b15f9b29ae53fad78291d76e56600
Jonas Moreira <jonasmoreira076@gmail.com>
2023-07-05 13:12:26 -0300

    Tipos de comentarios em Java

---
+++ java-terminal/src/Comentarios.java
@@ -0,0 +1,21 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // Olá, eu sou um comentário em uma única linha
+
+        /* Olá,
+         * Eu sou um comentário
+         * que posso ser mais detalhado
+         * quando necessário
+         */
+    }
+
+    /**
+     * Estas duas estrelinhas acima
+     * é para identificar que você
+     * pretende elaborar um comentário
+     * a nivel de documentação.
+     */
+    public void metodo(){
+
+    }
+}

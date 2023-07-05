0f141cefc373950df42dfc66f2265eeda36af801
Jonas Moreira <jonasmoreira076@gmail.com>
2023-07-05 14:36:03 -0300

    Criação da Documentação web e Util

--- java-terminal/src/Calculadora.java
+++ java-terminal/src/Calculadora.java
@@ -12,7 +12,7 @@
 
 public class Calculadora {
     /**
-    * Este método é utilizadfo para somar dois números inteiros 
+    * Este método é utilizado para somar dois números inteiros 
     * @param numeroUm este é o primeiro parâmetro do método
     * @param numeroDois este é o segundo parâmetro do método
     * @return int o resultado deste método é a soma dois números.

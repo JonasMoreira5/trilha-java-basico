25fe12d91683a6610811a37786b46dc44e552497
Jonas Moreira <jonasmoreira076@gmail.com>
2023-07-07 15:59:15 -0300

    Condicional Simples

--- controle-fluxo/src/CaixaEletronico.java
+++ controle-fluxo/src/CaixaEletronico.java
@@ -1,11 +1,12 @@
 public class CaixaEletronico {
     public static void main(  String[] args) {
         double saldo = 25.0;
-        double valorSolicitado = 17.0;
+        double valorSolicitado = 20.0;
 
-        if(valorSolicitado < saldo)
+        if(valorSolicitado < saldo){
             saldo = saldo - valorSolicitado;
-
+            System.out.println("Novo Saldo " + saldo);
+        }else
             System.out.println(saldo);
     }
 }
---
+++ controle-fluxo/src/resultadoEscolar.java
@@ -0,0 +1,10 @@
+public class resultadoEscolar {
+    public static void main(String[] args) {
+        int nota = 6;
+
+        if(nota >= 7)
+            System.out.println("Aprovado");
+        else
+            System.out.println("Reprovado"); 
+    }
+}

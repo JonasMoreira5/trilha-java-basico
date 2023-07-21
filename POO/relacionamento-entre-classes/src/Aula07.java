636f847ee1c58dbc9150b94232cfe1a1de650a6d
Jonas Moreira <jonasmoreira076@gmail.com>
2023-07-21 00:59:44 -0300

    Fazendo a ligação das duas classes

--- POO/relacionamento-entre-classes/src/Aula07.java
+++ POO/relacionamento-entre-classes/src/Aula07.java
@@ -7,8 +7,6 @@ public class Aula07 {
         l[3] = new Lutador("Dead Code", "Austrália", 28, 1.93f, 81.9f, 13, 0, 2);
         l[4] = new Lutador("UFOCobol", "Brasil", 37, 1.70f, 119.3f, 5, 4, 3);
         l[5] = new Lutador("Nerdaart", "EUA", 30, 1.81f, 105.7f, 12, 2, 4);
-        
-        l[3].Status();
 
     }
 }
--- POO/relacionamento-entre-classes/src/Luta.java
+++ POO/relacionamento-entre-classes/src/Luta.java
@@ -1,4 +1,4 @@
-import javax.lang.model.type.NullType;
+import java.util.Random;
 
 public class Luta {
     // atributos
@@ -9,44 +9,65 @@ public class Luta {
 
     // Métodos públicos
     public void marcarLuta(Lutador l1, Lutador l2){
-        if ((l1.getCategoria() == l2.getCategoria()) && (l1 != l2)){
+        if ((l1.getCategoria().equals(l2.getCategoria())) && l1 != l2){
             this.aprovada = true;
             this.desafiado = l1;
             this.desafiante = l2;
         } else{
             this.aprovada = false;
-            this.desafiado = NullType;
-            this.desafiante = NullType;
+            this.desafiado = null;
+            this.desafiante = null;
         }
     }
 
     // Falta implementar esse trecho
     public void lutar(){
         if (this.aprovada){
-            desafiado.apresentar();
-            desafiante.apresentar();
+            System.out.println("### DESAFIADO ###");
+            this.desafiado.apresentar();
+            System.out.println("### DESAFIANTE ###");
+            this.desafiante.apresentar();
             
+            // Escolhendo um numero aleatorio
+            Random aleatorio = new Random();
+            int vencedor = aleatorio.nextInt(3); // 0 1 2
+            
+            // adicionar Switch case  
         }else{
             System.out.println("Luta não pode acontecer");
         }
     }
 
     // Métodos especiais
-    public void setDesafiado(Lutador dd){
-        this.desafiado = dd;
+    public void setDesafiado(Lutador desafiado){
+        this.desafiado = desafiado;
     }
 
     public Lutador getDesafiado(){
         return desafiado;
     }
 
-    public void setDesafiante(Lutador dt){
-        this.desafiante = dt;
+    public void setDesafiante(Lutador desafiante){
+        this.desafiante = desafiante;
     }
 
     public Lutador getDesafiante(){
         return desafiante;
     }
 
-    
+    public void setAprovada(boolean aprovada){
+        this.aprovada = aprovada;
+    }
+
+    public boolean getAprovada(){
+        return aprovada;
+    }
+
+    public void setRounds(int rounds){
+        this.rounds = rounds;
+    }
+
+    public int getRounds(){
+        return rounds;
+    }
 }

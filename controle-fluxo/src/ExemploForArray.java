9a922191c2e4c256945dbba59cb9e1a494bfb752
Jonas Moreira <jonasmoreira076@gmail.com>
2023-07-28 13:30:01 -0300

    Uma forma mais abreviada de forArray

--- controle-fluxo/src/ExemploForArray.java
+++ controle-fluxo/src/ExemploForArray.java
@@ -3,8 +3,13 @@ public class ExemploForArray {
         // em arrays o indice inicia em ZERO
         String alunos [] = {"FELIPE", "JONAS", "JULIA", "MARCOS"};
 
-        for (int x=0; x<alunos.length; x++){
+        /*for (int x=0; x<alunos.length; x++){
             System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+        }*/
+        
+        // uma forma de interagir com elementos de um array
+        for(String aluno : alunos ){
+            System.out.println("Nome do aluno eh: " + aluno);
         }
     }
 }

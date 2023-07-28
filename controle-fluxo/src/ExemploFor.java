c4f1bdac6cbbe06fcd18865820fdc98e4c7fdcda
Jonas Moreira <jonasmoreira076@gmail.com>
2023-07-28 13:25:41 -0300

    for para arrays

---
+++ controle-fluxo/src/ExemploFor.java
@@ -0,0 +1,7 @@
+public class ExemploFor {
+    public static void main(String[] args) throws Exception {
+        for (int carneirinho = 1; carneirinho <=20; carneirinho++){
+            System.out.println("Contando Carneirinhos: " + carneirinho);
+        }
+    }
+}
---
+++ controle-fluxo/src/ExemploForArray.java
@@ -0,0 +1,10 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        // em arrays o indice inicia em ZERO
+        String alunos [] = {"FELIPE", "JONAS", "JULIA", "MARCOS"};
+
+        for (int x=0; x<alunos.length; x++){
+            System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+        }
+    }
+}

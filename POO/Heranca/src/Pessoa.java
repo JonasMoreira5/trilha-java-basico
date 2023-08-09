93882230ec1eb03a60c7667b2d013dd9531d10ec
Jonas Moreira <jonasmoreira076@gmail.com>
2023-08-09 20:08:15 -0300

    Foi criado a classe pessoa com seus atributos e me

---
+++ POO/Heranca/src/Pessoa.java
@@ -0,0 +1,32 @@
+public class Pessoa {
+    // atributos
+    private String nome;
+    private int idade;
+    private String sexo;
+
+    // Métodos
+    public void fazerAniv(){
+
+    }
+
+    // Métodos especiais
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
+    public int getIdade(){
+        return idade;
+    }
+
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+
+    public String getSexo(){
+        return sexo;
+    }
+}

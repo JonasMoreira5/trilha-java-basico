ba0594fc86562c49c11920fd56d1b77c404a99ec
Jonas Moreira <jonasmoreira076@gmail.com>
2023-08-10 00:05:24 -0300

    Interação de objetos com a classe

--- POO/Heranca/src/ProjetoPessoas.java
+++ POO/Heranca/src/ProjetoPessoas.java
@@ -1,5 +1,18 @@
 public class ProjetoPessoas {
     public static void main(String[] args) throws Exception {
-        // System.out.println("Hello, World!");
+        Pessoa p1 = new Pessoa();
+        Aluno p2 = new Aluno();
+        Professor p3 = new Professor();
+        Funcionario p4 = new Funcionario();
+        
+        p1.setNome("Pedro");
+        p2.setNome("Maria");
+        p3.setNome("Claudio");
+        p4.setNome("Fabiana");
+
+        System.out.println(p1.toString());
+        System.out.println(p2.toString());
+        System.out.println(p3.toString());
+        System.out.println(p4.toString());
     }
 }

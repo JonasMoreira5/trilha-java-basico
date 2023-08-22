cecd7997add5bc6334a6d79012ebf29031cce76c
Jonas Moreira <jonasmoreira076@gmail.com>
2023-08-22 16:53:53 -0300

    feat: adicionado Seleção de candidadtos

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,12 +1,35 @@
 package candidatura;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
-        System.out.println("Processo Seletivo");
-        analizarCandidato(1900.0);
-        analizarCandidato(2200.0);
-        analizarCandidato(2000.0);
+        selecaoCandidatos();
 
+    }
+
+    static void selecaoCandidatos(){
+        // Array com a lista de candidatos
+        String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"};
+        
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+
+        while(candidatosSelecionados < 5){
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + "Solicitou este valor de salário " + salarioPretendido);
+            if(salarioBase >= salarioPretendido){
+                System.out.println("O cadidato " + candidato + "foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+            candidatoAtual++;
+        }
+    }
+
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
     }
 
     static void analizarCandidato(double salarioPretendido){

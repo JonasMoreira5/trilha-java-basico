8e29f19dd7a7805b683dd7f55efb0a562c819d83
Jonas Moreira <jonasmoreira076@gmail.com>
2023-08-28 17:38:47 -0300

    feat: add funcinalidades

---
+++ controle-fluxo/DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) throws Exception {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parametro: ");
+        int parametroUm = terminal.nextInt();
+
+        System.out.println("Digite o segundo parametro: ");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            // chamando o método contendo a lógica de contagem
+            
+        } catch (Exception e) {
+            // imprimir a mensagem: Os segundo parâmetro dever ser maior que o primeiro
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois){
+        
+    }
+}
